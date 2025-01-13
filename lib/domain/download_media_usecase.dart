f65e95654b1ebc3b61fbc6a3d53ae7bf3ba2f683
arda-copur <ardacopur0@gmail.com>
2025-01-13 22:26:33 +0300

    Toaster

--- lib/domain/download_media_usecase.dart
+++ lib/domain/download_media_usecase.dart
@@ -9,19 +9,19 @@ class DownloadMediaUseCase {
     final fileName = mediaPath.split('/').last;
     final filePath = '${directory.path}/$fileName';
 
-    // Görüntüyü yükle
+   
     final imageFile = File(mediaPath);
     final imageBytes = await imageFile.readAsBytes();
     final image = img.decodeImage(imageBytes);
 
-    // Görüntüyü sıkıştır
+
     final compressedImage = img.encodeJpg(image!, quality: 85);
 
-    // Sıkıştırılmış görüntüyü kaydet
+  
     final compressedFile = File(filePath);
     await compressedFile.writeAsBytes(compressedImage);
 
-    // Galeriye kaydet
+
     final result = await ImageGallerySaver.saveFile(filePath);
     print("File saved to gallery: $result");
 
